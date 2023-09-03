213efa01d467752e5addf5a37069706d79e485ad
WalterNickSuelArroyo <nicodehero21@outlook.com>
2023-09-03 02:02:32 -0500

    validaciones de campos

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -38,6 +38,7 @@ class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Column(
         children: [
           TextFormField(
@@ -48,6 +49,13 @@ class _LoginForm extends StatelessWidget {
               labelText: 'Correo institucional',
               prefixIcon: Icons.alternate_email_rounded
             ),
+            validator: (value) {
+              String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp  = RegExp(pattern);
+              return regExp.hasMatch(value ?? '')
+                ? null
+                : 'Ingrese un correo correcto';
+            },
           ),
           const SizedBox(height: 30),
           TextFormField(
@@ -59,6 +67,11 @@ class _LoginForm extends StatelessWidget {
               labelText: 'Contraseña',
               prefixIcon: Icons.lock_outline
             ),
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                ? null
+                : 'La contraseña debe de ser de 6 caracteres';
+            },
           ),
           const SizedBox(height: 30),
           MaterialButton(
