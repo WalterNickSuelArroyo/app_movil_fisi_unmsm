f925643d44b5fce92dcdb22389ee3ccfba2a473b
leonidas <leonidas1808@hotmail.com>
2023-10-18 13:36:48 -0500

    vista nuevocurso 50%

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_movil_fisi_unmsm/src/pages/asignacionAula/asignacionAula_page.dart';
 import 'package:app_movil_fisi_unmsm/src/pages/login/login_page.dart';
+import 'package:app_movil_fisi_unmsm/src/pages/nuevo_curso/nuevo_curso.dart';
 import 'package:app_movil_fisi_unmsm/src/pages/pabellon/pabellon_page.dart';
 import 'package:app_movil_fisi_unmsm/src/pages/piso/piso_page.dart';
 import 'package:flutter/material.dart';
@@ -26,12 +27,13 @@ class _MyAppState extends State<MyApp> {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fisi Help',
-      initialRoute: '/',
+      initialRoute: '/nuevoCurso',
       getPages: [
         GetPage(name: '/', page: () => LoginPage(),),
         GetPage(name: '/pabellon', page: () => const PabellonPage(),),
         GetPage(name: '/piso', page: () => const PisoPage(),),
-        GetPage(name: '/asignacionaula', page: () => AsignacionAulaPage(),)
+        GetPage(name: '/asignacionaula', page: () => AsignacionAulaPage(),),
+        GetPage(name: '/nuevoCurso', page: ()=> const nuevoCurso(),),
       ],
       theme: ThemeData(
         primaryColor: const Color.fromRGBO(252, 147, 64, 1),
---
+++ lib/src/pages/nuevo_curso/nuevo_curso.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// ignore: camel_case_types
+class nuevoCurso extends StatelessWidget{
+  const nuevoCurso({super.key});
+
+    
+    @override
+    Widget build(BuildContext context){
+        return Scaffold(
+            body: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        //widgets hijos
+                        _imagenTitulo(),
+                        _titulo(),
+                        _subtitulo(),
+                        _opcionEscuela(),
+                    ]
+                )
+            )
+        );
+    }
+}
+
+Widget _imagenTitulo(){
+    return SafeArea(
+        child: Container(
+            margin: const EdgeInsets.only(top: 90),
+            alignment: Alignment.center,
+            child: SvgPicture.asset('assets/img/img_nuevoCurso.svg'),
+            ),
+    );
+}
+
+Widget _titulo(){
+    return Container(
+        margin: const EdgeInsets.only(top: 45),
+        alignment: Alignment.center,
+        child: Text(
+            'Nuevo Curso',
+            style: GoogleFonts.montserrat(
+                color: const Color.fromRGBO(16, 54, 95, 1),
+                fontSize: 45,
+                fontWeight: FontWeight.w800,
+            ),
+        ),
+    );
+}
+
+Widget _subtitulo(){
+    return Container(
+        margin: const EdgeInsets.only(top: 45),
+        alignment: Alignment.center,
+        child: Text(
+            'Ingrese esta información',
+            style: GoogleFonts.montserrat(
+                color: const Color.fromRGBO(16, 54, 95, 1),
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+            ),
+        ),
+    );
+}
+
+Widget _opcionEscuela(){
+    return Container(
+        margin: const EdgeInsets.only(top: 45),
+        alignment: Alignment.center,
+        child: Column(
+            children: [
+                Row(
+                    children: [
+                        SvgPicture.asset('assets/img/carta.svg'),
+                        const SizedBox(height: 16.0),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                                Text(
+                                    'Escuela',
+                                    style: GoogleFonts.montserrat(
+                                        color: const Color.fromRGBO(16, 54, 95, 1),
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w500,
+                                    ),
+                                ),
+                                Text(
+                                    'Ingenieria de Software',
+                                    style: GoogleFonts.montserrat(
+                                        color: const Color.fromRGBO(77, 77, 77, 1),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w100,
+                                    ),
+                                )
+                            ],
+                        )
+                    ],
+                )
+            ]
+        ),
+    );
+}
